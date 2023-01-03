7af20d3f2d6a3be2f96f95442f19e4e0e781703f
Sara Szwejkowska <saraszw@op.pl>
2023-01-03 21:18:53 +0100

    created a function to see which is was clicked and to check if checkbox is clicked

--- script.js
+++ script.js
@@ -7,35 +7,50 @@ async function getData() {
 	return data;
 }
 getData().then((data) => {
-    const comments=data.slice(0,20)
-    comments.forEach(element => {
-        // dodać checkbox do tabeli i zbierać zaznaczone
-        tableBody.innerHTML += `
+	const comments = data.slice(0, 20);
+
+	comments.forEach((element) => {
+		// dodać checkbox do tabeli i zbierać zaznaczone
+
+		tableBody.innerHTML += `
+  
         <tr id='comment${element.id}'>
+        <td><input data-id={${element.id}} name={${element.name}}  type='checkbox'/></td>
         <td>${element.id}</td>
 		<td>${element.name}</td>
         <td>${element.email}</td>
         <td>${element.body}</td>
         <td><button data-id='comment${element.id}'>Usun</button></td>
 		</tr>
-        `
-    });
-    // checkbox - 2
+        `;
+	});
 
-    const buttons = Array.from(document.querySelectorAll('button'));
-    
-buttons.forEach((button)=> {
+	const buttons = Array.from(document.querySelectorAll('button'));
+	const inputs = Array.from(document.querySelectorAll('input'));
 
-button.addEventListener("click",(e)=>{
-    
-    // złapać po id tr
-    const commentToRemove = document.querySelector(`#${e.target.dataset.id}`)
-    // wyrzucić
-    
-commentToRemove.remove();
-})
+	console.log(inputs);
 
-})
+	// let result = function resultInputs() {
+	//     inputs.filter()
+	// }
+	let idToDelay = [];
+	inputs.forEach((input) => {
+		input.addEventListener('change', (e) => {
+			if (e.target.checked) {
+				idToDelay.push(e.target.dataset.id);
+				console.log(idToDelay);
+			} else {
+				idToDelay = idToDelay.filter((item) => item !== e.target.dataset.id);
+				console.log(idToDelay);
+			}
+		});
+	});
 
-
-})
+	buttons.forEach((button) => {
+		button.addEventListener('click', (e) => {
+			const commentToRemove = document.querySelector(`#${e.target.dataset.id}`);
+			commentToRemove.remove();
+		});
+	});
+});
+// przefiltrowac po tablicy te zaznaczone i je wyrzucic
